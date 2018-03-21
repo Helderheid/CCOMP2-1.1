46290eef7515a9298bd455a12d595f37ad524eda
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-03-21 09:13:09 -0500

    modificando hello world

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x,y;
+    cout << "el primer numero es: ";cin>>x;
+    cout << "el segundo numero es: ";cin>>y;
+    cout << "la suma es: "<<x+y<<endl;
+    cout << "la resta es: "<<x-y<<endl;
+    cout << "la multiplicacion es: "<<x*y<<endl;
+    cout << "la division es: "<<x/y<<endl;
     return 0;
+
 }
